2d743aa630501339c06afc7454acc6d2657b1467
mrsalmanahmad <salmanahmad3333@gmail.com>
2020-01-26 22:00:24 +0500

    added items api

--- todos-api/app/controllers/todos_controller.rb
+++ todos-api/app/controllers/todos_controller.rb
@@ -3,9 +3,41 @@ class TodosController < ApplicationController
 
 	#GET /todos
 	def index
-		@todo = Todo.all
-		json_response(@todo)
+		@todos = Todo.all
+		json_response(@todos)
 	end
 
 	#POST /todos
+	def create
+		@todo = Todo.create!(todo_params)
+		json_response(@todo, :created)
+	end
+
+	#GET /todos/:id
+	def show
+		json_response(@todo)
+	end
+
+	#PUT /todos/:id
+	def update
+		@todo.update(todo_params)
+		head :no_content
+	end
+
+	#Destory /todos/:id
+	def destroy
+		@todo.destroy
+		head :no_content
+	end
+
+	private
+
+	def todo_params
+		#whitelist params
+		params.permit(:title, :created_by)
+	end
+
+	def set_todo
+		@todo = Todo.find(params[:id])
+	end
 end
