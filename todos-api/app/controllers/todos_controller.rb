4e955db7e2eec98c2a2e6f5c84721e8461708030
mrsalmanahmad <salmanahmad3333@gmail.com>
2020-01-26 18:20:47 +0500

    new api

--- todos-api/app/controllers/todos_controller.rb
+++ todos-api/app/controllers/todos_controller.rb
@@ -1,2 +1,11 @@
 class TodosController < ApplicationController
+	before_action :set_todo, only:[:show,:update,:destroy]
+
+	#GET /todos
+	def index
+		@todo = Todo.all
+		json_response(@todo)
+	end
+
+	#POST /todos
 end
